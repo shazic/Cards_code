1abd312dbb719733b6d4c9f1f32b9928a8826166
Shashank Chattopadhyaya <Schattopadhyaya@nextgen.com>
2022-11-19 12:22:48 +0530

    place conftest.py under the tests/ root

--- practice/cards_proj/tests/ch03/conftest.py
+++ practice/cards_proj/tests/ch03/conftest.py
@@ -1,48 +1,48 @@
-import pytest
-import cards
-from pathlib import Path
-from tempfile import TemporaryDirectory
+# import pytest
+# import cards
+# from pathlib import Path
+# from tempfile import TemporaryDirectory
 
-def pytest_addoption(parser):
-    parser.addoption(
-        '--func-db',
-        action='store_true',
-        default=False,
-        help='new db for each test',
-    )
+# def pytest_addoption(parser):
+#     parser.addoption(
+#         '--func-db',
+#         action='store_true',
+#         default=False,
+#         help='new db for each test',
+#     )
 
-def db_scope(fixture_name, config):
-    if config.getoption("--func-db", None):
-        return "function"
-    return "session"
+# def db_scope(fixture_name, config):
+#     if config.getoption("--func-db", None):
+#         return "function"
+#     return "session"
 
-@pytest.fixture(scope=db_scope)
-def db():
-    """CardsDB object connected to a temporary database
-    """
-    with TemporaryDirectory() as db_dir:
-        db_path = Path(db_dir)
-        db_ = cards.CardsDB(db_path)
-        yield db_
-        db_.close()
+# @pytest.fixture(scope=db_scope)
+# def db():
+#     """CardsDB object connected to a temporary database
+#     """
+#     with TemporaryDirectory() as db_dir:
+#         db_path = Path(db_dir)
+#         db_ = cards.CardsDB(db_path)
+#         yield db_
+#         db_.close()
 
-@pytest.fixture(scope="session")
-def some_cards():
-    return [
-        cards.Card("write book", "Brian", "done"),
-        cards.Card("edit book", "Katie", "done"),
-        cards.Card("write 2nd edition", "Brian", "todo"),
-        cards.Card("edit 2nd edition", "Katie", "todo"),
-    ]
+# @pytest.fixture(scope="session")
+# def some_cards():
+#     return [
+#         cards.Card("write book", "Brian", "done"),
+#         cards.Card("edit book", "Katie", "done"),
+#         cards.Card("write 2nd edition", "Brian", "todo"),
+#         cards.Card("edit 2nd edition", "Katie", "todo"),
+#     ]
 
-@pytest.fixture(scope="function")
-def non_empty_db(cards_db, some_cards):
-    for c in some_cards:
-        cards_db.add_card(c)
-    return cards_db
+# @pytest.fixture(scope="function")
+# def non_empty_db(cards_db, some_cards):
+#     for c in some_cards:
+#         cards_db.add_card(c)
+#     return cards_db
 
-@pytest.fixture(scope="function")
-def cards_db(db):
-    """CardsDB object that is empty"""
-    db.delete_all()
-    return db
+# @pytest.fixture(scope="function")
+# def cards_db(db):
+#     """CardsDB object that is empty"""
+#     db.delete_all()
+#     return db
